5373adbec8505960c42e57cde3b7eca9218746ca
saku <pg.saku0719@gmail.com>
2024-03-24 21:09:56 +0900

    [fix] getEventの機能修正

--- src/hooks/firebase/getEvent.ts
+++ src/hooks/firebase/getEvent.ts
@@ -1,27 +1,15 @@
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { DocumentData, doc, getDoc } from "firebase/firestore";
 import { db } from "./init";
 import { collectionName } from "../../constants/const";
 
-export const getEvent = async (isAll: boolean, eventIds: string[]) => {
-  if (isAll) {
-    const collectionRef = collection(db, collectionName);
-    await getDocs(collectionRef)
-      .then((querySnapshot) => {
-        return querySnapshot.docs.map((doc) => ({ ...doc.data() }));
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
-        return [];
-      });
-  } else {
-    const docRef = doc(db, collectionName, eventIds[0]);
-    await getDoc(docRef)
-      .then((docSnap) => {
-        return docSnap.exists() ? docSnap.data() : [];
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
-        return [];
-      });
+export const getEvent = async (eventId: string): Promise<DocumentData> => {
+  try {
+    const docRef = doc(db, collectionName, eventId);
+    const snapshot = await getDoc(docRef);
+
+    return snapshot.data() ?? [];
+  } catch (error) {
+    console.error("Error getting documents: ", error);
+    return [];
   }
 };
