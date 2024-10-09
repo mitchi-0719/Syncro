7375020441ad64859685e2ba90c3baeb6cc1b9a8
mitchi <pg.saku0719@gmail.com>
2024-10-09 20:12:34 +0900

    [delete] いろいろ削除

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material";
 export const Header = () => {
   return (
     <Box component="header" py={2}>
-      <Typography variant="h1" align="center" sx={{fontSize: "2rem"}}>
+      <Typography variant="h1" align="center" sx={{ fontSize: "2rem" }}>
         Header
       </Typography>
     </Box>
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,25 +1,8 @@
 import { Box, Typography } from "@mui/material";
 import { EventCreator } from "../components/Top/EventCreator";
 import { EventBoard } from "../components/common/EventBoard";
-// import { useEffect, useState } from "react";
 
 export const Top = () => {
-  // const [cookieData, setCookieData] = useState<string[]>([]);
-
-  // // dummy処理 最終的には消す
-  // useEffect(() => {
-  //   (async () => {
-  //     fetch("dummy/dummyCookieData.json")
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //         setCookieData(data["dummyCookies"]);
-  //       })
-  //       .catch((err) => {
-  //         console.error(err);
-  //       });
-  //   })();
-  // }, []);
-
   return (
     <Box>
       <Typography>イベントを作成する</Typography>
