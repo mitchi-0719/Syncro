ba5b9351343da75fdd8e2cd2076fc520dfaf55a4
mitchi <pg.saku0719@gmail.com>
2025-01-07 04:44:47 +0900

    [add] 非表示機能追加

--- src/components/eventBoard/PastSeenEventBoard.tsx
+++ src/components/eventBoard/PastSeenEventBoard.tsx
@@ -2,6 +2,7 @@ import { Box, Divider, Typography } from "@mui/material";
 import { EventBoard } from "./EventBoard";
 import { getSeenEventIdList } from "../../function/localStorage/seenEventIdList";
 import { isEqualArray } from "../../function/isEqualArray";
+import { isNullOrUndefinedOrEmptyArray } from "../../function/isNullOrUndefined";
 
 type Props = {
   currentEventid?: string;
@@ -10,7 +11,11 @@ type Props = {
 export const PastSeenEventBoard = ({ currentEventid }: Props) => {
   const seenEventIdList = getSeenEventIdList();
 
-  if (isEqualArray(seenEventIdList, [currentEventid], true)) return null;
+  if (
+    isNullOrUndefinedOrEmptyArray(seenEventIdList) ||
+    isEqualArray(seenEventIdList, [currentEventid], true)
+  )
+    return null;
   return (
     <Box marginY={4}>
       <Typography variant="h5">過去に開いたイベント</Typography>
